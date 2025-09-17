42c4452cca5a72b615605b0d423f823b079fb0b4
Adam Ma <adam.ma@klaviyo.com>
2025-09-16 22:29:14 -0400

    Move Endpoint

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -54,6 +54,26 @@ def validate_email(email: str) -> tuple[bool, str]:
     return True, "Email is valid"
 
 
+# Get current user
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception from None
+    user = await user_model.get_by_email(email)
+    if user is None:
+        raise credentials_exception
+    return UserInDB(**user)
+
+
 @router.post("/", response_model=UserResponse)
 async def create_user(
     username: Annotated[str, Form(...)],
@@ -190,26 +210,6 @@ async def oauth_scheme_token(form_data: Annotated[OAuth2PasswordRequestForm, Dep
         data={"sub": user["email"]}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
-
-
-# Get current user
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-        email: str = payload.get("sub")
-        if email is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception from None
-    user = await user_model.get_by_email(email)
-    if user is None:
-        raise credentials_exception
-    return UserInDB(**user)
 
 
 # Protected route to get current user info
