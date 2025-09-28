675a7c3590b4d65b3d5ccbcc715a250b1e881c63
Afnan Tuffaha <tuffaha.af@northeastern.edu>
2025-09-28 00:58:00 -0400

    update registration schema

--- app/schemas/registration.py
+++ app/schemas/registration.py
@@ -1,12 +1,23 @@
+from datetime import datetime
+from enum import Enum
+
 from pydantic import BaseModel
+
+
+class RegistrationStatus(str, Enum):
+    UPCOMING = "upcoming"
+    COMPLETED = "completed"
+    INCOMPLETED = "incompleted"
 
 
 class Registration(BaseModel):
     id: str
-    first_name: str
-    last_name: str
-    username: str
+    event_id: str
     volunteer_id: str
+    registered_at: datetime
+    registration_status: RegistrationStatus
+    clocked_in: bool
+    clocked_out: bool
 
     class Config:
         from_attributes = True
