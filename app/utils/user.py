39c6cbe79a2601438cb6c258318dd8e91d054559
qihongw08 <qihongw08@gmail.com>
2025-09-29 18:09:31 -0400

    get top organization for current user

--- app/utils/user.py
+++ app/utils/user.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta
 
+from fastapi import HTTPException, status
 from jose import jwt
 from passlib.context import CryptContext
 
 from app.core.config import settings
+from app.schemas.user import User, UserType
 
 # Password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -27,3 +29,19 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
+
+
+def verify_user_role(user: User, required_role: UserType):
+    if user.user_type != required_role:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Unuthorized to access this resource",
+        )
+
+
+def verify_entity_association(user: User):
+    if user.entity_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Missing entity association",
+        )
