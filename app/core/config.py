0a8956db8bb6057e4fc913430c1f259691fbbb42
Adam Ma <adam.ma@klaviyo.com>
2025-12-03 22:31:57 -0500

    Fix

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "Karp DB"
     API_V1_STR: str = "/api/v1"
     MONGODB_URL: str
-    DATABASE_NAME: str = "newest_karp_db"
+    DATABASE_NAME: str
     SECRET_KEY: str = "karp"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int
